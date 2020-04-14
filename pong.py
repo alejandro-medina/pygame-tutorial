a7b143df27ff6417e73ac76e20fac576d41c0123
Alejandro Medina <rodrigo.medinacetz@gmail.com>
2020-04-14 01:03:45 -0500

    Add movement to the sprite

--- pong.py
+++ pong.py
@@ -13,11 +13,21 @@ HEIGHT = 480
 class Ball(pygame.sprite.Sprite):
 	def __init__(self):
 		pygame.sprite.Sprite.__init__(self)
-		self.image = load_image('images/ball.png')
+		self.image = load_image('images/ball.png', True)
 		self.rect = self.image.get_rect()
 		self.rect.centerx = WIDTH / 2
 		self.rect.centery = HEIGHT / 2
 		self.speed = [0.5, -0.5]
+
+	def update(self, time):
+		self.rect.centerx += self.speed[0] * time
+		self.rect.centery += self.speed[1] * time
+		if self.rect.left <= 0 or self.rect.right >= WIDTH:
+			self.speed[0] = -self.speed[0]
+			self.rect.centerx += self.speed[0] * time
+		if self.rect.bottom >= HEIGHT or self.rect.top <= 0:
+			self.speed[1] = -self.speed[1]
+			self.rect.centery += self.speed[1] * time
 
 # Functions
 
@@ -38,18 +48,26 @@ def main():
 	pygame.display.set_caption("Pong game")
 	
 	bg = load_image('images/pong_bg.png')
-	screen.blit(bg, (0, 0))
 
 	# Draw the ball
 	ball = Ball()
-	screen.blit(ball.image, ball.rect)
-	
-	pygame.display.flip()
+
+
+	clock = pygame.time.Clock()
 
 	while True:
+		# Exit the program
 		for event in pygame.event.get():
 			if event.type == QUIT:
 				sys.exit(0)
+
+		
+		time = clock.tick(60)
+		ball.update(time)
+
+		screen.blit(bg, (0, 0))
+		screen.blit(ball.image, ball.rect)
+		pygame.display.flip()
 
 if __name__ == '__main__':
 	pygame.init()
